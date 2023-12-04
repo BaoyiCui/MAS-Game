15f5251f667a7e8298f3d37f6f92b76fc7753a0a
Baoyi Cui <3239177665@qq.com>
2023-12-04 16:02:21 +0800

    添加了攻击者的分组初始化

--- Aerial Swarm Defense Using Interception and Herding Strategies/main.m
+++ Aerial Swarm Defense Using Interception and Herding Strategies/main.m
@@ -14,20 +14,44 @@ rho_d = 2500; % defenders' percepting region
 rho_d_game = 2000; % defenders' playing region
 rho_a_game = 2700; % attacker's initial position region
 radius_a = 0.5; % attacker's geometric radius
-radius_d = 0.5; % defender's
+radius_d = 0.5; % defender's geometric radius
+radius_c = 1.5; % clustered group's radius
 R_sb = 10; % string barrier's maximum length
 N_a = 20; % attackers' number
 N_d = 20; % defenders' number
+num_c = 3;  % number of clusters
+num_uc = 3; % number of unclustered attackers
 
 %% initialize the agents
 attackers = cell(1, N_a);
 defenders = cell(1, N_d);
 % initialize the attackers
-pos_init_a = randomPointsInRing(rho_d, rho_a_game, N_a);
+group_tags_c = randi([1, num_c], 1, N_a - num_uc); % clustered attackers的组标签
+group_tags_uc = (num_c + 1):(num_c + num_uc); % unclustered attackers的组标签
+% 最后几个attacker为unclustered attacker
+% unclustered attacker的group tag为-1
+group_tags = [group_tags_c, group_tags_uc];
+
+% 生成每个clustered 和unclustered group的中心坐标
+pos_group_init_a = randomPointsInRing(rho_d, rho_a_game, num_c + num_uc);
 
 for i = 1:N_a
-    attackers{i}.position = pos_init_a(:, i);
+    group_tag = group_tags(i);
+
+    if i <= N_a - num_uc
+        c_center = pos_group_init_a(:, group_tag);
+        attackers{i}.position = randomPointsInCircle(radius_c, 1) + c_center;
+    else
+        attackers{i}.position = pos_group_init_a(:, group_tag);
+    end
+
 end
+
+% pos_init_a = randomPointsInRing(rho_d, rho_a_game, N_a);
+
+% for i = 1:N_a
+%     attackers{i}.position = pos_init_a(:, i);
+% end
 
 % initialize the defenders
 pos_init_d = randomPointsInCircle(rho_d_game, N_d);
